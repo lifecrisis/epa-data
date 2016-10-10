b21348f965e8eb6d83ccfba2c3cafbbdd4b5c15c
Jason Franklin <j_fra@fastmail.us>
2016-10-10 18:45:22 -0400

    Implemented test and method to load ozone files.

---
+++ epa/clean.py
@@ -0,0 +1,39 @@
+"""
+Clean and amalgamate EPA data for analysis.
+"""
+
+
+import os.path
+
+
+# Project directory root.
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Data directory roots.
+DATA_ROOT = os.path.join(PROJECT_ROOT, 'data/')
+OZONE_DATA_ROOT = os.path.join(DATA_ROOT, 'ozone_raw_data/')
+PM_25_DATA_ROOT = os.path.join(DATA_ROOT, 'pm_25_raw_data/')
+
+
+def load_data_file(pollutant, year):
+    """
+    Return a data file handle for the given pollutant ('ozone' or 'pm')
+    and year.
+    """
+
+    # Select the correct file or raise an IOError.
+    if pollutant == 'ozone' and year in range(1990, 2016):
+        path_to_file = os.path.join(OZONE_DATA_ROOT,
+                                    "daily_44201_" + str(year) +".csv")
+        result = open(path_to_file, 'r')
+    elif pollutant == 'pm' and year in range(1990, 2016):
+        path_to_file = os.path.join(PM_25_DATA_ROOT,
+                                    "daily_88101_" + str(year) +".csv")
+        result = open(path_to_file, 'r')
+    else:
+        msg = "Data file with type '" + pollutant + "' and year '" +\
+              str(year) + "' not found."
+        raise IOError(msg)
+
+    # Return a handle to the selected file.
+    return result
---
+++ test/test_clean.py
@@ -0,0 +1,53 @@
+"""
+Test the epa/clean.py module.
+
+This module provides unit tests that ensure that the routines for cleaning
+our EPA data work as expected.
+"""
+
+# TODO(jf): Test reading, then manipulating, then writing for each file set.
+# TODO(jf): Test your code on the first/last lines of first/last files in both
+#           sets.
+
+
+import glob
+import os.path
+import random
+import unittest
+
+from epa import clean
+
+
+class LoadDataTestCase(unittest.TestCase):
+    """
+    Test that all data files can be successfully loaded.
+
+    Specifically, test "DATA_ROOT" constants and "load_data_file(type, year)"
+    method.
+    """
+
+    def test_load_ozone_files(self):
+        """ Test that ozone data files can be loaded. """
+
+        # Test accuracy of OZONE_DATA_ROOT.
+        ozone_pathnames = glob.glob(clean.OZONE_DATA_ROOT + '/daily_*.csv')
+        self.assertEqual(len(ozone_pathnames), 26)
+        for pathname in ozone_pathnames:
+            self.assertTrue(os.path.exists(pathname))
+
+        # Test that files can, in fact, be loaded into memory.
+        ozone_pathnames = random.sample(ozone_pathnames, 5)
+        for pathname in ozone_pathnames:
+            year = int(pathname[-8:-4])
+            ozone_file = clean.load_data_file('ozone', year)
+            self.assertIsInstance(ozone_file, file)
+            self.assertGreater(len(list(ozone_file)), 10000)
+            ozone_file.close()
+
+        # Test that an incorrectly specified file raises an Error.
+        self.assertRaises(IOError, clean.load_data_file, 'xxxxx', 2020)
+        self.assertRaises(IOError, clean.load_data_file, 'xxxxx', 1995)
+        self.assertRaises(IOError, clean.load_data_file, 'ozone', 1980)
+        self.assertRaises(IOError, clean.load_data_file, 'ozone', 2020)
+
+    # TODO(jf): Write test for loading PM2.5 data.
